5bb360c0eec827618ed3799f7ecd1742200d5cc9
302ehrhr <302ehrhr@gmail.com>
2022-04-22 05:54:06 +0000

    2022-04-22 sever update

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -116,8 +116,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -152,7 +150,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+AWS_ACCESS_KEY_ID = 'AKIARCIZISC6QEB3E2EW'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'picktalk'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+            'CacheControl': 'max-age=86400',
+            }
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static'),
+                '/usr/local/lib/python3.8/dist-packages/django/contrib/admin'
+                ]
+STATIC_URL = 'https://%s/%s/static/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'picktalk.storage_backends.MediaStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3.8 manage.py runserver 0.0.0.0:8000
+python3.8 manage.py runserver
