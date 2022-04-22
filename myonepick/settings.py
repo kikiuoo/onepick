38fd7a0399f936903ee5fd038f3e6753e817f662
J <jar2003@naver.com>
2022-04-22 16:01:50 +0900

    2022_04_22 kakao Login Test 7

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -179,7 +179,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATIC_ROOT = '/usr/local/lib/python3.8/dist-packages/django/contrib/admin'
 
 STATIC_URL = 'static/'
 
