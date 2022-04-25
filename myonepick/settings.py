bf4c0e8c5772092d7455e892ff441dbd350f16d9
J <jar2003@naver.com>
2022-04-25 10:56:31 +0900

    2022_04_25 서버 복구 테스트

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-"""
+""" """
 AWS_ACCESS_KEY_ID = 'AKIARCIZISC6QEB3E2EW'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'picktalk'
@@ -171,8 +171,8 @@ DEFAULT_FILE_STORAGE = 'picktalk.storage_backends.MediaStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-"""
 
+"""
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -186,7 +186,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-"""
+
 ## 카카오 키들은 나중에 accounts.view에서 쓰일 예정
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
