524be2d910fef47368c8d68eb1bd1873888c9767
J <jar2003@naver.com>
2022-05-25 15:33:01 +0900

    2022-05-25 애플 로그인 테스트3

--- user/urls.py
+++ user/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('login/google/', views.googleLogin, name='google-login'),  # 구글 리다이렉트.
     path('login/google/callback/', views.googleLoginCallback, name='google-callback'),  # 구글 리다이렉트.
 
+    #path('login/apple/', views.appleLogin, name='apple-login'),  # 애플 리다이렉트.
+    path('login/apple/callback/', views.appleLoginCallback, name='apple-callback'),  # 애플 리다이렉트.
+
     path('login/local/', views.localLogin, name='local-login'), # 로컬 로그인.
     path('logout/local/', views.locallogout, name='local-logout'), # 로컬 로그아웃.
 
