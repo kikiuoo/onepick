e5833f9edf0243118b31424039c9a7c3f5961a10
J <jar2003@naver.com>
2022-06-10 15:59:17 +0900

    2022-06-10 세팅 변경8

--- startServer.py
+++ startServer.py
@@ -1,18 +1,19 @@
 import psutil
+import os
 
 
 for q in psutil.process_iter():
     try:
         print(q.name())
-        if q.name() == 'python':
+        if q.name() == 'python3.8':
             if len(q.cmdline()) > 1 and 'manage.py' in q.cmdline()[1]:
-                print("존재")
+                os.system("python /home/joe/PyCharmProj/SocServer.py &")
                 
             else :
                 print("없음")
+
         else :
             print("없음2")
-
 
     except psutil.AccessDenied:
         continue
