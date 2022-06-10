e724bee6e41e11b72f6834785f27a6c25908fd8f
J <jar2003@naver.com>
2022-06-10 15:57:09 +0900

    2022-06-10 세팅 변경7

--- startServer.py
+++ startServer.py
@@ -3,6 +3,7 @@ import psutil
 
 for q in psutil.process_iter():
     try:
+        print(q.name())
         if q.name() == 'python':
             if len(q.cmdline()) > 1 and 'manage.py' in q.cmdline()[1]:
                 print("존재")
