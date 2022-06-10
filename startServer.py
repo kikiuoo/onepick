55a659976b35d015d58c4df7b3018f367e9d53aa
J <jar2003@naver.com>
2022-06-10 15:53:34 +0900

    2022-06-10 세팅 변경4

--- startServer.py
+++ startServer.py
@@ -3,13 +3,14 @@ import psutil
 
 for q in psutil.process_iter():
     try:
-        if q.name() == 'manage.py':
-            if len(q.cmdline()) > 1 and '실행하고 있는 python 파일 이름' in q.cmdline()[1]:
+        if q.name() == 'python.exe':
+            if len(q.cmdline()) > 1 and 'manage.py' in q.cmdline()[1]:
                 print("존재")
                 
             else :
                 print("없음")
-                
+
+
 
     except psutil.AccessDenied:
         continue
