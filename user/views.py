6c62d1224b3a55e6a39c4cb62d58c9e35dccbcf1
J <jar2003@naver.com>
2022-06-13 17:23:49 +0900

    2022-06-13 네이버 로그인2

--- user/views.py
+++ user/views.py
@@ -17,7 +17,7 @@ from django.conf import settings
 import boto3
 from django.db import connection
 
-loginUrl = "https://ksnpick.com/users/login"
+loginUrl = "http://ksnpick.com/users/login"
 #loginUrl = "http://localhost:8000/users/login"
 
 
