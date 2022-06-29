8faea224a694c2f6edcb89d9a3a88daa8251019b
J <jar2003@naver.com>
2022-06-29 15:48:52 +0900

    2022-06-29 서버세팅 2

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+#STATIC_ROOT = os.path.join(BASE_DIR,"static")
 STATIC_ROOT = '/usr/local/lib/python3.8/dist-packages/django/contrib/admin'
 
 MEDIA_URL = '/media/'
