8350cbd04467467a9204a4d604badfae2df6c508
J <jar2003@naver.com>
2022-06-29 18:22:49 +0900

    2022-06-29 서버세팅 2

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -145,11 +145,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
-#STATIC_ROOT = os.path.join(BASE_DIR,"static")
-STATIC_ROOT = '/usr/local/lib/python3.8/dist-packages/django/contrib/admin'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
