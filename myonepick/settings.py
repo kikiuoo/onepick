eb8931c653e93bd196485d8fdbc899f5ebc139bf
J <jar2003@naver.com>
2022-07-05 15:17:47 +0900

    2022-07-05 오디션 오류 수정

--- myonepick/settings.py
+++ myonepick/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
-    #'crosheaders',
+    'crosheaders',
 
     'picktalk.apps.PicktalkConfig',
     'user',
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'corsheaders.middleware.CorsMiddleware', #최상단에 추가해주기
+    'corsheaders.middleware.CorsMiddleware', #최상단에 추가해주기
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
