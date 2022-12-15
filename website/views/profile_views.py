b4362c5d0b55d08eb4b9c87aaa81672716e3c52f
J <jar2003@naver.com>
2022-12-15 17:31:27 +0900

    2022-12-15 - 프로필 등록 오류

--- website/views/profile_views.py
+++ website/views/profile_views.py
@@ -660,7 +660,7 @@ def pofile_write_callback(request) :
                                              skincolor=skinColor, haircolor=hairColor, foreign=saveForeign, mainyoutube=youtube_main,
                                              youtube=sYoutube, talent=saveSpecialty, comment=introduction, intercate=cate_m,
                                              intersubcate=cate_s, iscareer=notCareer, careeryear=allCareer_y, careermonth=allCareer_m, tag = saveTag, mbti=mbti,
-                                             regdate=nowTime, update=nowTime, viewcount=0, cviewcount=0, public=notView,isdelete=0, pickcount=0, clsscount=0 )
+                                             regdate=nowTime, update=nowTime, viewcount=0, cviewcount=0, public=notView,isdelete=0, pickcount=0, classcount=0 )
 
     key = str(ProfileInfo.objects.latest('num').num)
 
